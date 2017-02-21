96a86c2776c57e860933c7bbb016b332108b5256
Edivan de Castro <edivandecastro@gmail.com>
2017-02-21 16:39:39 -0300

    first commit

---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative 'boot'
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Highlander
+  class Application < Rails::Application
+    config.generators do |g|
+      g.orm :mongoid
+    end
+  end
+end
